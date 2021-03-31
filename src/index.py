1ead42e780ae7ef32cac3c81dfed495af3c943f3
Mikael <mikael.vilen@gmail.com>
2021-03-31 22:34:59 +0300

    added pygame to dependencies

--- src/index.py
+++ src/index.py
@@ -1,1 +1,12 @@
-print('ping pong')
+import pygame, sys
+
+size = width, height = 800, 600
+print(size)
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    screen.fill((90, 160, 240))
+    pygame.display.flip()
--- tuntikirjanpito.md
+++ tuntikirjanpito.md
@@ -1,2 +1,2 @@
 31.3, projektin alustus, 1 tunti
-
+31.3, pygamen käyttönotto ja perustoimintoihin tutustuminen, 1 tunti
